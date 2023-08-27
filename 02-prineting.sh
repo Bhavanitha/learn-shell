705794e4d2492a63e7f1f9dc1a69147a3fd21cc5
Bhavana <vbhavanitha@gmail.com>
2023-08-27 16:22:44 +0530

    colours

--- 02-prineting.sh
+++ 02-prineting.sh
@@ -1,3 +1,12 @@
 # To print a message there are couple of commands, echo is widely used one.
 
 echo Hello bhavanitha!!
+
+#print in some color
+#syntax: echo -e "\e[COLmMESSAGE\e[0m"
+# -e - enable colors
+#\e[COLm - to enable certain color
+# \e[0m - to disable color which is enabled
+# COL stands for color and possible colors are  RED(31), GREEN(32), YELLOW(33), BLUE(34), MAGENTA(35),CYAN(36)
+echo -e "\e[31mhello in red\e[0m"
+
